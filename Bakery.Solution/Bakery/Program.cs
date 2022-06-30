875cf7fd815962c1020d30698551141fc4140e00
Adrienne <100542795+ampando@users.noreply.github.com>
2022-06-30 14:24:15 -0700

    inital setup for project.

---
+++ Bakery.Solution/Bakery/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BakeryShop
+{
+  public class Program
+  {
+    public static void Main()
+    {
+    
+    }
+  }
+}  

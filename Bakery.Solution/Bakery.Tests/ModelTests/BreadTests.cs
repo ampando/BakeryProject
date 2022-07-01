44ff4597152c9d1983e1a7e110759721c0ea157f
Adrienne <100542795+ampando@users.noreply.github.com>
2022-06-30 18:34:51 -0700

    failed constructor test.

--- Bakery.Solution/Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Solution/Bakery.Tests/ModelTests/BreadTests.cs
@@ -1,3 +1,43 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_ConstructAnInstanceOfBread_Bread_Bread()
+    {
+      Bread newBread = new Bread(2);
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+    [TestMethod]
+    public void GetPrice_ReturnsPrice_Int()
+    {
+      int testPrice = 5;
+      Bread newBread = new Bread(2);
+      int result = newBread.Price;
+      Assert.AreEqual(testPrice, result);
+    }
+    [TestMethod]
+    public void GetNumberOfLoaves_ReturnNumberOfLoaves_Int()
+    {
+      int testLoaves = 2;
+      Bread newBread = new Bread(testLoaves);
+      int result = newBread.NumberOfLoaves;
+      Assert.AreEqual(testLoaves, result);
+    }
+    [TestMethod]
+    public void TotalBreadPrice_ReturnsPriceBasedOnNumberOfLoaves_Int()
+    {
+      int testLoaves = 2;
+      int testPrice = 10;
+      Bread newBread = new Bread(testLoaves);
+      newBread.TotalBreadPrice();
+      int result = newBread.Price;
+      Assert.AreEqual(testPrice, result);
+    }
+  }
+}
--- Bakery.Solution/Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Solution/Bakery.Tests/ModelTests/PastryTests.cs
@@ -1,3 +1,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    // Test methods go here
+  }
+}

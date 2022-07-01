b9576c6e53a989453002f20fd2cb40cbfd5384ec
Adrienne <100542795+ampando@users.noreply.github.com>
2022-06-30 22:42:35 -0700

    failing test for bread total return price.

--- Bakery.Solution/Bakery/Models/Breads.cs
+++ Bakery.Solution/Bakery/Models/Breads.cs
@@ -4,17 +4,35 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    public int Price { get; set; }
     public int NumberOfLoaves { get; set; }
-    
+    public int Price { get; set; }
     public Bread(int numberOfLoaves)
     {
       Price = 5;
       NumberOfLoaves = numberOfLoaves;
     }
-    public void TotalBreadPrice()
+    //public void TotalBreadPrice()
+    
+    public bool IsOnlyPositiveNumberCharacters()
     {
-      Price = (Price * NumberOfLoaves);
+      if (NumberOfLoaves > 0)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    
+    public int TotalBreadPrice()
+    {
+      int newTotal = Price * NumberOfLoaves;
+      if (NumberOfLoaves > 2)
+      {
+        newTotal -= ((NumberOfLoaves/3)*5);
+      }
+      return newTotal;
     }
   }
 }
--- Bakery.Solution/Bakery/Program.cs
+++ Bakery.Solution/Bakery/Program.cs
@@ -10,15 +10,25 @@ namespace BakeryShop
     public static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery");
-      Console.WriteLine("Our loaves of bread are $5 or choose 3 for #10 and our pastries are $2 each or 3 for $5");
+      Console.WriteLine("Our loaves of bread are $5 or choose 3 for $10. Our pastries are $2 each or 3 for $5");
       Start();
     }
     public static void Start()
     {
       Console.WriteLine("How many loaves would you like?");
       Bread newBread = new Bread(int.Parse(Console.ReadLine()));
-    }
+
+      if (newBread.IsOnlyPositiveNumberCharacters())
+      {
+        Console.WriteLine("Here is your bread order total: $" + (newBread.TotalBreadPrice()));
+      }
+      else
+      {
+        Console.WriteLine("Please enter a positive number");
+        Start();
       }
     }
+  }
+}
   
 

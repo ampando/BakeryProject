74cbadabdfa997bd46ecf6df0ffeeeffda07b0f7
Adrienne <100542795+ampando@users.noreply.github.com>
2022-06-30 17:32:18 -0700

    welcome greeting working.

--- Bakery.Solution/Bakery/Models/Breads.cs
+++ Bakery.Solution/Bakery/Models/Breads.cs
@@ -6,6 +6,7 @@ namespace Bakery.Models
   {
     public int Price { get; set; }
     public int NumberOfLoaves { get; set; }
+    
     public Bread(int numberOfLoaves)
     {
       Price = 5;
--- Bakery.Solution/Bakery/Program.cs
+++ Bakery.Solution/Bakery/Program.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using Bakery.Models;
 
 namespace BakeryShop
@@ -9,11 +9,16 @@ namespace BakeryShop
   {
     public static void Main()
     {
-   
-    
-    
-    
+      Console.WriteLine("Welcome to Pierre's Bakery");
+      Console.WriteLine("Our loaves of bread are $5 or choose 3 for #10 and our pastries are $2 each or 3 for $5");
+      Start();
+    }
+    public static void Start()
+    {
+      Console.WriteLine("How many loaves would you like?");
+      Bread newBread = new Bread(int.Parse(Console.ReadLine()));
+    }
       }
     }
-  }
+  
 
